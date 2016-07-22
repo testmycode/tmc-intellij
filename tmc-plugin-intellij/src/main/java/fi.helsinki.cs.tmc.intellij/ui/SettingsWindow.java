e51e483eee4acc0c053248e5a157b9ad7cfe50f8
Denopia <miika.leinonen@helsinki.fi>
2016-07-22 13:47:26 +0300

    tmc settings button added

---
+++ tmc-plugin-intellij/src/main/java/fi.helsinki.cs.tmc.intellij/ui/SettingsWindow.java
@@ -0,0 +1,26 @@
+package fi.helsinki.cs.tmc.intellij.ui.elements;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SettingsWindow {
+
+    JFrame frame;
+
+
+    public SettingsWindow(){
+        frame = new JFrame();
+        JPanel panel = new SettingsPanel(frame).getPanel();
+        frame.setSize(new Dimension(700, 400));
+        frame.add(panel);
+        frame.setTitle("TMC Settings");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setSize(new Dimension(700, 400));
+    }
+
+}
