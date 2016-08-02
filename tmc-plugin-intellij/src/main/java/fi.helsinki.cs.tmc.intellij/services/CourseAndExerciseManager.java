86178e52c65fe5188a31eb833ce71d26a59fd5b9
Samu Kauppinen <samu.kauppinen@helsinki.fi>
2016-08-02 12:34:12 +0300

    added error message popup if tmc settings are not initialized or internet connection is not found

--- tmc-plugin-intellij/src/main/java/fi.helsinki.cs.tmc.intellij/services/CourseAndExerciseManager.java
+++ tmc-plugin-intellij/src/main/java/fi.helsinki.cs.tmc.intellij/services/CourseAndExerciseManager.java
@@ -111,9 +111,13 @@ public class CourseAndExerciseManager {
                 }
             }
         } catch (TmcCoreException exception) {
-            Messages.showErrorDialog(new ObjectFinder().findCurrentProject(),
+            StartupErrorMessageService s = new StartupErrorMessageService();
+            s.showMessage(exception);
+
+            /*Messages.showErrorDialog(new ObjectFinder().findCurrentProject(),
                     exception.getMessage()
                     + " " + exception.toString(), "Error");
+            */
         }
     }
 
