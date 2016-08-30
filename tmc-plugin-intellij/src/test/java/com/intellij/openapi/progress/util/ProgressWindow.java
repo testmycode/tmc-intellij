8f2ae754a9e0ae5d3e97d2f35cc7c32ae95c325e
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-08-30 13:05:42 +0300

    temp fix for mvn tests

---
+++ tmc-plugin-intellij/src/test/java/com/intellij/openapi/progress/util/ProgressWindow.java
@@ -0,0 +1,8 @@
+package com.intellij.openapi.progress.util;
+
+/*
+This is a simple workaround for tests. For whatever reason Mockito
+fails at creating a mock of ThreadingService.
+ */
+public interface ProgressWindow {
+}
