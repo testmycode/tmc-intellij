422a5f9ccb267a8377891f2fb5aacbe7058fce43
Denopia <m.leinonen@hotmail.com>
2016-08-30 13:40:21 +0300

    loggers added

--- tmc-plugin-intellij/src/main/java/fi/helsinki/cs/tmc/intellij/io/CoreProgressObserver.java
+++ tmc-plugin-intellij/src/main/java/fi/helsinki/cs/tmc/intellij/io/CoreProgressObserver.java
@@ -4,9 +4,13 @@ import fi.helsinki.cs.tmc.core.domain.ProgressObserver;
 
 import com.intellij.openapi.progress.util.ProgressWindow;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CoreProgressObserver extends ProgressObserver{
 
     private final ProgressWindow progressWindow;
+    private static final Logger logger = LoggerFactory.getLogger(CoreProgressObserver.class);
 
     public CoreProgressObserver(ProgressWindow progressWindow) {
         this.progressWindow = progressWindow;
@@ -14,12 +18,14 @@ public class CoreProgressObserver extends ProgressObserver{
 
     @Override
     public void progress(long mysteryLong, String status) {
+        logger.info("Setting progress status. @CoreProgressObserver");
         progressWindow.setText2(status);
         progressWindow.checkCanceled();
     }
 
     @Override
     public void progress(long mysteryLong, Double progress, String status) {
+        logger.info("Setting progress status. @CoreProgressObserver");
         progressWindow.setText2(status);
         progressWindow.setFraction(progress);
         progressWindow.checkCanceled();
@@ -27,11 +33,13 @@ public class CoreProgressObserver extends ProgressObserver{
 
     @Override
     public void start(long mysteryLong) {
+        logger.info("Opening progress window. @CoreProgressObserver");
         progressWindow.start();
     }
 
     @Override
     public void end(long mysteryLong) {
+        logger.info("Closing progress window. @CoreProgressObserver");
         progressWindow.dispose();
     }
 }
