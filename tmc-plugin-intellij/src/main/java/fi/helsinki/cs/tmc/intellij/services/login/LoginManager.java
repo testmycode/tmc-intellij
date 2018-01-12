3f0761f2d069296d472c7c5d5c95b90682e49fe0
vilmakoo <vilma.l.kangas@helsinki.fi>
2018-01-12 13:32:02 +0200

    Give better error messages for malformed server addresses

--- tmc-plugin-intellij/src/main/java/fi/helsinki/cs/tmc/intellij/services/login/LoginManager.java
+++ tmc-plugin-intellij/src/main/java/fi/helsinki/cs/tmc/intellij/services/login/LoginManager.java
@@ -2,10 +2,12 @@ package fi.helsinki.cs.tmc.intellij.services.login;
 
 import com.google.common.base.Optional;
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.ui.Messages;
 import fi.helsinki.cs.tmc.core.domain.ProgressObserver;
 import fi.helsinki.cs.tmc.core.exceptions.AuthenticationFailedException;
 import fi.helsinki.cs.tmc.core.exceptions.TmcCoreException;
 import fi.helsinki.cs.tmc.intellij.holders.TmcCoreHolder;
+import fi.helsinki.cs.tmc.intellij.holders.TmcSettingsManager;
 import fi.helsinki.cs.tmc.intellij.io.SettingsTmc;
 import fi.helsinki.cs.tmc.intellij.services.errors.ErrorMessageService;
 import fi.helsinki.cs.tmc.intellij.services.persistence.PersistentTmcSettings;
@@ -23,7 +25,6 @@ public class LoginManager {
 
     private static final Logger logger = LoggerFactory.getLogger(LoginManager.class);
 
-
     public boolean login(String password) {
         ErrorMessageService errorMessageService = new ErrorMessageService();
 
@@ -34,16 +35,34 @@ public class LoginManager {
             return true;
         } catch (Exception e) {
             if (e instanceof IOException) {
-                connectionException = (IOException)e;
+                connectionException = (IOException) e;
                 if (e instanceof UnknownHostException) {
-                    errorMessageService.showHumanReadableErrorMessage((TmcCoreException)e, true);
+                    errorMessageService.showErrorMessage(
+                            e,
+                            "Couldn't connect to the server. Please check your internet connection.",
+                            true);
                 } else if (e instanceof FileNotFoundException) {
-                    errorMessageService.showErrorMessage(e, "Server address is incorrect.", true);
+                    if (TmcSettingsManager.get().getServerAddress().contains("tmc.mooc.fi/mooc")) {
+                        Messages.showErrorDialog(
+                                "The server https://tmc.mooc.fi/mooc is no longer supported by this client.\n"
+                                        + "All the courses have been migrated to our main server https://tmc.mooc.fi.\n"
+                                        + "If you'd like to do the migrated courses, you'll need to update your server address to\n"
+                                        + "https://tmc.mooc.fi and create a new account there.\n"
+                                        + "After that, choose the MOOC organization after logging in.\n\n"
+                                        + "For more information, check the course materials on mooc.fi.",
+                                "Error");
+                    } else {
+                        errorMessageService.showErrorMessage(
+                                e,
+                                "Server address is incorrect or the server is not supported.",
+                                true);
+                    }
                 }
-            }
-            if (e instanceof AuthenticationFailedException) {
+            } else if (e instanceof AuthenticationFailedException) {
                 authenticationException = (AuthenticationFailedException) e;
                 errorMessageService.showErrorMessage(e, "Username or password is incorrect", true);
+            } else {
+                Messages.showErrorDialog("Logging in failed! Try again.", "Error");
             }
 
             if (authenticationException != null) {
@@ -70,7 +89,8 @@ public class LoginManager {
 
         final PersistentTmcSettings saveSettings =
                 ServiceManager.getService(PersistentTmcSettings.class);
-        final SettingsTmc settings = ServiceManager.getService(PersistentTmcSettings.class).getSettingsTmc();
+        final SettingsTmc settings =
+                ServiceManager.getService(PersistentTmcSettings.class).getSettingsTmc();
 
         settings.setToken(Optional.absent());
         settings.setOauthCredentials(Optional.absent());
