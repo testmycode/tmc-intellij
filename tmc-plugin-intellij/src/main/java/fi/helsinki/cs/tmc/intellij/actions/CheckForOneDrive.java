d1f2baf241be10ca82d1f6ccefe8a3ae628c729c
vilmakoo <vilma.l.kangas@helsinki.fi>
2018-04-19 16:30:13 +0300

    Modify error message again

--- tmc-plugin-intellij/src/main/java/fi/helsinki/cs/tmc/intellij/actions/CheckForOneDrive.java
+++ tmc-plugin-intellij/src/main/java/fi/helsinki/cs/tmc/intellij/actions/CheckForOneDrive.java
@@ -17,7 +17,7 @@ public class CheckForOneDrive {
         String message = "OneDrive does not work with the TestMyCode plugin for IntelliJ\n\n" +
             "Many students have had a problem when they have their IntelliJ project folder in OneDrive, which results in submissions failing on the server. \n" +
             "The problem can be solved by moving the project folder out of OneDrive as follows:\n\n" +
-            "- Copy or move the \"IdeaProjects\" folder to some path that does not contain the \"OneDrive\" folder.\n" +
+            "- Copy or move the \"TMCProjects\" folder to some path that does not contain the \"OneDrive\" folder.\n" +
             "- Change the IntelliJ project folder to the path you selected in the previous step by selecting \"TMC\" -> \"Settings\".\n" +
             "- Now you can open the moved exercises by selecting \"File\" -> \"Open...\" and selecting the exercises from the new folder.";
         errorMessageService.showErrorMessagePopup(message);
